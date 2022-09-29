975d7911928ed8057260a63149a57a73a602aaa3
Gatz22 <mgatchair@gmail.com>
2022-09-29 14:13:23 -0400

    I type slow sorry

--- assignments/week 2/inventorydb.sql
+++ assignments/week 2/inventorydb.sql
@@ -1,6 +1,6 @@
 -- Created our inventory table
 CREATE TABLE inventory(
-	ID serial PRIMARY KEY,
+	ID int PRIMARY KEY,
 	"name" varchar(25),
 	price NUMERIC,
 	description varchar(60),
@@ -35,6 +35,41 @@ WHERE ID = 2005;
 -- Updating the M&M stocks
 UPDATE inventory
 SET quantity = 100
-WHERE ID = 9007
+WHERE ID = 9007;
 
 -- updating lolipops
+UPDATE inventory
+SET price = 2.50
+WHERE ID = 3008;
+
+-- creating employees table
+CREATE TABLE employees(
+	ID serial,
+	"name" varchar(25),
+	favorite_candy int REFERENCES inventory
+);
+
+-- adding data to the employees table
+INSERT INTO employees
+VALUES
+	(4001,'Willy Wonka', 2005),
+	(4002,'Milton Hershey', 1001),
+	(4003,'Franklin Mars', 9007),
+	(4004,'John Cadbury', null);
+	
+-- running query for favorite candy minus null
+SELECT "name", favorite_candy FROM employees
+WHERE favorite_candy IS NOT NULL;
+
+--joining to print out all favorite candies from employees 
+
+SELECT i."name", e."name"
+FROM inventory i 
+LEFT JOIN employees e  ON i.id = favorite_candy;
+
+-- bonus question 1
+SELECT "name",
+price*quantity AS total_price
+FROM inventory
+WHERE id = 9007;
+--bonus question 2
